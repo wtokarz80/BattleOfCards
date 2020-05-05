b5a0151a618ed5b8ff9eb60e6f68a8a1589bf09b
Michal Myczka <michalmorgoth@gmail.com>
2020-05-05 14:02:12 +0200

    merging

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -5,6 +5,30 @@ public class HumanPlayer extends Player {
 
     @Override
     void chooseStatistic() {
+        boolean isRunning = true;
+        while(isRunning){
+            System.out.println("Choose Statistic That You Want To Use:\n (1)strength\n(2)intelligence\n(3)agility\n(4)charisma\n");
+            int userInput = Ui.getNumericInput();
+            if(userInput == 1){
+                isRunning = false;
+            }
+            else if(userInput == 2){
+                isRunning = false;
+            }
+            else if(userInput == 3){
+                isRunning = false;
+            }
+            else if(userInput == 4){
+                isRunning = false;
+            }
+            else{
+                System.out.println("Choose Only Allowed Option: \n");
+            }
+        }
+    }
+
+    public static void playTheGame(){
+        Ui.displayMenu();
 
     }
 }
